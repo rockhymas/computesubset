0b24d9f973ed33d6d398608a393e4378a0df114e
Rock Hymas <rock@hymasfamily.org>
2022-05-31 16:00:10 -0600

    Initial Commit

---
+++ ComputeSubset/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ComputeSubset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var typeface = new Typeface(new FontFamily("Segoe UI"), FontStyles.Normal, FontWeights.SemiBold, FontStretches.Normal);
+
+            if (!typeface.TryGetGlyphTypeface(out var glyphTypeFace))
+            {
+                return;
+            }
+
+            glyphTypeFace.ComputeSubset(glyphTypeFace.AdvanceHeights.Keys);
+
+            Console.WriteLine("Success!");
+        }
+    }
+}
